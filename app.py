6bf215098cf021a8f5324b7cf293aa17c6398df9
Mudaliar Saurabh Ravi <96583960+MudaliarSaurabh@users.noreply.github.com>
2024-12-14 10:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def send_message_to_model(message, image_path):
 # Streamlit app
 def main():
     st.title("Gemini 1.5 Pro, UI to Code 👨‍💻 ")
-    st.subheader('Made with ❤️ by [Skirano](https://x.com/skirano)')
+    st.subheader('Made by Mudaliar Saurabh)
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
